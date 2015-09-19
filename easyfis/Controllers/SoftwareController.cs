8ddb2740bd076ae98108f82132ca3081cf14d70e
oliverrigonan <oliverrigonan@gmail.com>
2015-09-19 14:13:12 +0800

    ERP: Software Controller / Authorize Annotation

--- easyfis/Controllers/SoftwareController.cs
+++ easyfis/Controllers/SoftwareController.cs
@@ -8,100 +8,148 @@ namespace easyfis.Controllers
 {
     public class SoftwareController : UserAccountController
     {
-        // GET: Software
+        // =============
+        // GET: SOFTWARE
+        // =============
         [Authorize]
         public ActionResult Index()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Supplier()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult PurchaseOrder()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult ReceivingReceipt()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult AccountsPayable()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Disbursement()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Bank()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Customer()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Sales()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult AccountsReceivable()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Collection()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult BankReconciliation()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Items()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult StockIn()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult StockOut()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult InventoryReport()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult StockTransfer()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult SystemTables()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult ChartOfAccounts()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult JournalVoucher()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Company()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult FinancialStatements()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Users()
         {
             return View();
         }
+
+        [Authorize]
         public ActionResult Settings()
         {
             return View();
