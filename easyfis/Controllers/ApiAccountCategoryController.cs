39d7ad468e0bb1add8c21238649f56b8a7aa3caf
paulbatuigas <paulbatuigas1995@gmail.com>
2015-10-10 15:36:10 -0700

    ERP Commit: Charge of Accounts Model and Api Controller

---
+++ easyfis/Controllers/ApiAccountCategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace easyfis.Controllers
+{
+    public class ApiAccountCategoryController : ApiController
+    {
+        // LIST
+        // LIST BY ID
+        // UPDATE
+        // DELETE
+    }
+}
---
+++ easyfis/Models/MstAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace easyfis.Models
+{
+    public class MstAccount
+    {
+        [Key]
+        public Int32 Id { get; set; }
+        public String AccountCode { get; set; }
+        public String Account { get; set; }
+        public Int32 AccountTypeId { get; set; }
+        public Int32 AccountCashFlowId { get; set; }
+        public Boolean IsLocked { get; set; }
+        public Int32 CreatedById { get; set; }
+        public String CreatedByIdUserName { get; set; }
+        public String CreatedDateTime { get; set; }
+        public Int32 UpdatedById { get; set; }
+        public String UpdatedByIdUserName { get; set; }
+        public String UpdatedDateTime { get; set; }
+    }
+}
