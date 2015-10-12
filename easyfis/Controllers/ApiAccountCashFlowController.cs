4c0252a8b7a68b4bac7e4ae7e4b2ed3afc9f0708
NoahOliverRigonan <oliverrigonan@gmail.com>
2015-10-13 01:24:48 +0800

    ERP Commit: Chart of Accounts (All done the CRUD)

--- easyfis/Controllers/ApiAccountCashFlowController.cs
+++ easyfis/Controllers/ApiAccountCashFlowController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
 namespace easyfis.Controllers
 {
@@ -31,6 +32,112 @@ namespace easyfis.Controllers
                                         UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
                                     };
             return accountCashFlows.ToList();
+        }
+
+        // =====================
+        // ADD Account Cash Flow
+        // =====================
+        [Route("api/addAccountCashFlow")]
+        public int Post(Models.MstAccountCashFlow accountCashFlow)
+        {
+            try
+            {
+                var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                Data.MstAccountCashFlow newAccountCashFlow = new Data.MstAccountCashFlow();
+
+                newAccountCashFlow.AccountCashFlowCode = accountCashFlow.AccountCashFlowCode;
+                newAccountCashFlow.AccountCashFlow = accountCashFlow.AccountCashFlow;
+                newAccountCashFlow.IsLocked = isLocked;
+                newAccountCashFlow.CreatedById = mstUserId;
+                newAccountCashFlow.CreatedDateTime = date;
+                newAccountCashFlow.UpdatedById = mstUserId;
+                newAccountCashFlow.UpdatedDateTime = date;
+
+                db.MstAccountCashFlows.InsertOnSubmit(newAccountCashFlow);
+                db.SubmitChanges();
+
+                return newAccountCashFlow.Id;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        // ========================
+        // UPDATE Account Cash Flow
+        // ========================
+        [Route("api/updateAccountCashFlow/{id}")]
+        public HttpResponseMessage Put(String id, Models.MstAccountCashFlow accountCashFlow)
+        {
+            try
+            {
+                var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                var accountCashFlowId = Convert.ToInt32(id);
+                var accountCashFlows = from d in db.MstAccountCashFlows where d.Id == accountCashFlowId select d;
+
+                if (accountCashFlows.Any())
+                {
+                    var updateAccountCashFlow = accountCashFlows.FirstOrDefault();
+
+                    updateAccountCashFlow.AccountCashFlowCode = accountCashFlow.AccountCashFlowCode;
+                    updateAccountCashFlow.AccountCashFlow = accountCashFlow.AccountCashFlow;
+                    updateAccountCashFlow.IsLocked = isLocked;
+                    updateAccountCashFlow.UpdatedById = mstUserId;
+                    updateAccountCashFlow.UpdatedDateTime = date;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // ========================
+        // DELETE Account Cash Flow
+        // ========================
+        [Route("api/deleteAccountCashFlow/{id}")]
+        public HttpResponseMessage Delete(String id)
+        {
+            try
+            {
+                var accountCashFlowId = Convert.ToInt32(id);
+                var accountCashFlows = from d in db.MstAccountCashFlows where d.Id == accountCashFlowId select d;
+
+                if (accountCashFlows.Any())
+                {
+                    db.MstAccountCashFlows.DeleteOnSubmit(accountCashFlows.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
--- easyfis/Controllers/ApiAccountCategoryController.cs
+++ easyfis/Controllers/ApiAccountCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
 namespace easyfis.Controllers
 {
@@ -31,6 +32,112 @@ namespace easyfis.Controllers
                                    UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
                                };
             return accountCategories.ToList();
+        }
+
+        // ====================
+        // ADD Account Category
+        // ====================
+        [Route("api/addAccountCategory")]
+        public int Post(Models.MstAccountCategory accountCategory)
+        {
+            try
+            {
+                var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                Data.MstAccountCategory newAccountCategory = new Data.MstAccountCategory();
+
+                newAccountCategory.AccountCategoryCode = accountCategory.AccountCategoryCode;
+                newAccountCategory.AccountCategory = accountCategory.AccountCategory;
+                newAccountCategory.IsLocked = isLocked;
+                newAccountCategory.CreatedById = mstUserId;
+                newAccountCategory.CreatedDateTime = date;
+                newAccountCategory.UpdatedById = mstUserId;
+                newAccountCategory.UpdatedDateTime = date;
+
+                db.MstAccountCategories.InsertOnSubmit(newAccountCategory);
+                db.SubmitChanges();
+
+                return newAccountCategory.Id;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        // =======================
+        // UPDATE Account Category
+        // =======================
+        [Route("api/updateAccountCategory/{id}")]
+        public HttpResponseMessage Put(String id, Models.MstAccountCategory accountCategory)
+        {
+            try
+            {
+                var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                var accountCategoryId = Convert.ToInt32(id);
+                var accountCategories = from d in db.MstAccountCategories where d.Id == accountCategoryId select d;
+
+                if (accountCategories.Any())
+                {
+                    var updateAccountCategory = accountCategories.FirstOrDefault();
+
+                    updateAccountCategory.AccountCategoryCode = accountCategory.AccountCategoryCode;
+                    updateAccountCategory.AccountCategory = accountCategory.AccountCategory;
+                    updateAccountCategory.IsLocked = isLocked;
+                    updateAccountCategory.UpdatedById = mstUserId;
+                    updateAccountCategory.UpdatedDateTime = date;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // =======================
+        // DELETE Account Category
+        // =======================
+        [Route("api/deleteAccountCategory/{id}")]
+        public HttpResponseMessage Delete(String id)
+        {
+            try
+            {
+                var accountCategoryId = Convert.ToInt32(id);
+                var accountCategories = from d in db.MstAccountCategories where d.Id == accountCategoryId select d;
+
+                if (accountCategories.Any())
+                {
+                    db.MstAccountCategories.DeleteOnSubmit(accountCategories.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
