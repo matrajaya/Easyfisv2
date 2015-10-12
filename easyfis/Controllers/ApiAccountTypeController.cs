277d9d72083fe269ab7d3cdb163a45c827a2e5bb
oliverrigonan <oliverrigonan@gmail.com>
2015-10-12 14:58:15 +0800

    ERP Commit: Updated Chart Of Accounts (Done CRUD of Accounts and Done CRUD of Account Type)

--- easyfis/Controllers/ApiAccountTypeController.cs
+++ easyfis/Controllers/ApiAccountTypeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
 namespace easyfis.Controllers
 {
@@ -34,6 +35,117 @@ namespace easyfis.Controllers
                                UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
                            };
             return accountTypes.ToList();
+        }
+
+
+        // ================
+        // ADD Account Type
+        // ================
+        [Route("api/addAccountType")]
+        public int Post(Models.MstAccountType accountType)
+        {
+            try
+            {
+                var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                Data.MstAccountType newAccountType = new Data.MstAccountType();
+
+                newAccountType.AccountTypeCode = accountType.AccountTypeCode;
+                newAccountType.AccountType = accountType.AccountType;
+                newAccountType.AccountCategoryId = accountType.AccountCategoryId;
+                newAccountType.SubCategoryDescription = accountType.SubCategoryDescription;
+                newAccountType.IsLocked = isLocked;
+                newAccountType.CreatedById = mstUserId;
+                newAccountType.CreatedDateTime = date;
+                newAccountType.UpdatedById = mstUserId;
+                newAccountType.UpdatedDateTime = date;
+
+                db.MstAccountTypes.InsertOnSubmit(newAccountType);
+                db.SubmitChanges();
+
+                return newAccountType.Id;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        // ===================
+        // UPDATE Account Type
+        // ===================
+        [Route("api/updateAccountType/{id}")]
+        public HttpResponseMessage Put(String id, Models.MstAccountType accountType)
+        {
+            try
+            {
+                var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                var accountTypeId = Convert.ToInt32(id);
+                var accountTypes = from d in db.MstAccountTypes where d.Id == accountTypeId select d;
+
+                if (accountTypes.Any())
+                {
+                    var updateAccountType = accountTypes.FirstOrDefault();
+
+                    updateAccountType.AccountTypeCode = accountType.AccountTypeCode;
+                    updateAccountType.AccountType = accountType.AccountType;
+                    updateAccountType.AccountCategoryId = accountType.AccountCategoryId;
+                    updateAccountType.SubCategoryDescription = accountType.SubCategoryDescription;
+                    updateAccountType.IsLocked = isLocked;
+                    updateAccountType.UpdatedById = mstUserId;
+                    updateAccountType.UpdatedDateTime = date;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // ==============
+        // DELETE Account
+        // ==============
+        [Route("api/deleteAccountType/{id}")]
+        public HttpResponseMessage Delete(String id)
+        {
+            try
+            {
+                var accountTypeId = Convert.ToInt32(id);
+                var accountTypes = from d in db.MstAccountTypes where d.Id == accountTypeId select d;
+
+                if (accountTypes.Any())
+                {
+                    db.MstAccountTypes.DeleteOnSubmit(accountTypes.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
