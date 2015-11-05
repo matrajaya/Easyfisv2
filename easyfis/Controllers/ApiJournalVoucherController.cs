fd94e047c154e4c7d979520815ed2b3766190132
AlsonnyCaña <masterstring101@gmail.com>
2015-11-05 15:59:47 -0800

    EasyFIS Commit: JVNumber and Buttons Modifications

--- easyfis/Controllers/ApiJournalVoucherController.cs
+++ easyfis/Controllers/ApiJournalVoucherController.cs
@@ -61,6 +61,39 @@ namespace easyfis.Controllers
             return journalVouchers.ToList();
         }
 
+        // ====================
+        // LIST Journal Voucher
+        // ====================
+        [Route("api/listJournalVoucherLast")]
+        public Models.TrnJournalVoucher GetLastDataId()
+        {
+            var journalVouchers = from d in db.TrnJournalVouchers.OrderByDescending(d => d.JVNumber)
+                                  select new Models.TrnJournalVoucher
+                                  {
+                                      Id = d.Id,
+                                      BranchId = d.BranchId,
+                                      Branch = d.MstBranch.Branch,
+                                      JVNumber = d.JVNumber,
+                                      JVDate = d.JVDate.ToShortDateString(),
+                                      Particulars = d.Particulars,
+                                      ManualJVNumber = d.ManualJVNumber,
+                                      PreparedById = d.PreparedById,
+                                      PreparedBy = d.MstUser3.UserName,
+                                      CheckedById = d.CheckedById,
+                                      CheckedBy = d.MstUser1.UserName,
+                                      ApprovedById = d.ApprovedById,
+                                      ApprovedBy = d.MstUser.UserName,
+                                      IsLocked = d.IsLocked,
+                                      CreatedById = d.CreatedById,
+                                      CreatedBy = d.MstUser2.UserName,
+                                      CreatedDateTime = d.CreatedDateTime.ToShortDateString(),
+                                      UpdatedById = d.UpdatedById,
+                                      UpdatedBy = d.MstUser4.UserName,
+                                      UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
+                                  };
+            return (Models.TrnJournalVoucher)journalVouchers.FirstOrDefault();
+        }
+
         // ==========================
         // LIST Journal Voucher by Id
         // ==========================
