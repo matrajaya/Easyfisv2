82d927451a092f0aeac58a38bcb07d830ac7f893
Alsonny Caña <masterstring101@gmail.com>
2015-12-07 17:03:56 +0800

    EeasyFIS Commit: Trn Inventory model

---
+++ easyfis/Models/TrnInventory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace easyfis.Models
+{
+    public class TrnInventory
+    {
+         [Key]
+        public Int32 Id { get; set; }
+        public Int32 BranchId { get; set; }
+        public String Branch { get; set; }
+        public String InventoryDate { get; set; }
+        public Int32 ArticleId { get; set; }
+        public Int32 ArticleInventoryId { get; set; }
+        public Int32 RRId { get; set; }
+        public Int32 SIId { get; set; }
+        public Int32 INId { get; set; }
+        public Int32 OTId { get; set; }
+        public Int32 STId { get; set; }
+        public Decimal QuantityIn { get; set; }
+        public Decimal Quantity { get; set; }
+        public Decimal QuantityOut { get; set; }
+        public Decimal Amount { get; set; }
+        public String Particulars { get; set; }
+
+
+    }
+}
