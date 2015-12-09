d3154f175214555e8ed125d0462dd184b9adf117
paulbatuigas <paulbatuigas1995@gmail.com>
2015-12-09 16:01:48 +0800

    easyfis commit: continue api listing

---
+++ easyfis/Controllers/ApiStockOutItemController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace easyfis.Controllers
+{
+    public class ApiStockOutItemController : ApiController
+    {
+        private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+        // ===================
+        // LIST Stock Out Item
+        // ===================
+        [Route("api/listStockOutItem")]
+        public List<Models.TrnStockOutItem> Get()
+        {
+            var stockOutItem = from d in db.TrnStockOutItems
+                              select new Models.TrnStockOutItem
+                              {
+                                  Id = d.Id,
+                                  //BranchId = d.BranchId,
+                                  //Branch = d.Branch,
+                                  OTId = d.OTId,
+                                  //OT = d.OT,
+                                  ExpenseAccountId = d.ExpenseAccountId,
+                                  //ExpenseAccount = d.ExpenseAccount,
+                                  ItemId = d.ItemId,
+                                  //Item = d.Item,
+                                  ItemInventoryId = d.ItemInventoryId,
+                                  //ItemInventory = d.ItemInventory,
+                                  Particulars = d.Particulars,
+                                  UnitId = d.UnitId,
+                                  //Unit = d.Unit,
+                                  Quantity = d.Quantity,
+                                  Cost = d.Cost,
+                                  Amount = d.Amount,
+                                  BaseUnitId = d.BaseUnitId,
+                                  //BaseUnit = d.BaseUnit,
+                                  BaseQuantity = d.BaseQuantity,
+                                  BaseCost = d.BaseCost        
+                              };
+            return stockOutItem.ToList();
+        }
+    }
+}
---
+++ easyfis/Controllers/ApiStockTransferController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace easyfis.Controllers
+{
+    public class ApiStockTransferController : ApiController
+    {
+        private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+        // ===================
+        // LIST Stock Transfer
+        // ===================
+        [Route("api/listStockTransfer")]
+        public List<Models.TrnStockTransfer> Get()
+        {
+            var stockTransfer = from d in db.TrnStockTransfers
+                                select new Models.TrnStockTransfer
+                                {
+                                    Id = d.Id,
+                                    //Userid = d.Userid,
+                                    //User = d.User,
+                                    //Audidate = d.Audidate,
+                                    TableInformation = d.TableInformation,
+                                    RecordInformation = d.RecordInformation,
+                                    FormInformation = d.FormInformation,
+                                    ActionInformation = d.ActionInformation
+                                };
+            return stockTransfer.ToList();
+        }
+    }
+}
