930b306d2d05d1ebd317fea7777de1f75b31f678
NoahOliverRigonan <oliverrigonan@gmail.com>
2015-12-14 01:27:17 +0800

    commit: collection

--- easyfis/Controllers/ApiCollectionController.cs
+++ easyfis/Controllers/ApiCollectionController.cs
@@ -22,27 +22,58 @@ namespace easyfis.Controllers
                               {
                                   Id = d.Id,
                                   BranchId = d.BranchId,
-                                  //Branch = d.Branch,
+                                  Branch = d.MstBranch.Branch,
                                   ORNumber = d.ORNumber,
-                                  //ORDate = d.ORDate,
-                                  //Customer = d.Customer,
+                                  ORDate = d.ORDate.ToShortDateString(),
+                                  CustomerId = d.CustomerId,
+                                  Customer = d.MstArticle.Article,
                                   Particulars = d.Particulars,
                                   ManualORNumber = d.ManualORNumber,
-                                  //PreparedBy = d.PreparedBy,
                                   PreparedById = d.PreparedById,
-                                  //CheckedBy = d.CheckedBy,
+                                  PreparedBy = d.MstUser3.FullName,
                                   CheckedById = d.CheckedById,
-                                  //ApprovedBy = d.ApprovedBy,
+                                  CheckedBy = d.MstUser.FullName,
                                   ApprovedById = d.ApprovedById,
+                                  ApprovedBy = d.MstUser1.FullName,
                                   IsLocked = d.IsLocked,
                                   CreatedById = d.CreatedById,
-                                  //CreatedBy = d.CreatedBy,
-                                  //CreatedDateTime = d.CreatedDateTime,
+                                  CreatedBy = d.MstUser2.FullName,
+                                  CreatedDateTime = d.CreatedDateTime.ToShortDateString(),
                                   UpdatedById = d.UpdatedById,
-                                  //UpdatedBy = d.UpdatedBy,
-                                  //UpdatedDateTime = d.UpdatedDateTime
+                                  UpdatedBy = d.MstUser4.FullName,
+                                  UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
                               };
             return collections.ToList();
+        }
+
+        // =================
+        // DELETE Collection
+        // =================
+        [Route("api/deleteCollection/{id}")]
+        public HttpResponseMessage Delete(String id)
+        {
+            try
+            {
+                var collectionId = Convert.ToInt32(id);
+                var collections = from d in db.TrnCollections where d.Id == collectionId select d;
+
+                if (collections.Any())
+                {
+                    db.TrnCollections.DeleteOnSubmit(collections.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
