b3f06cad96630a7ea739803db33a413bc6cf3491
NoahOliverRigonan <oliverrigonan@gmail.com>
2015-12-19 00:06:53 +0800

    EasyFIS Commit: RR Retrieve

--- easyfis/Controllers/ApiReceivingReceiptController.cs
+++ easyfis/Controllers/ApiReceivingReceiptController.cs
@@ -56,6 +56,53 @@ namespace easyfis.Controllers
             return receivingReceipts.ToList();
         }
 
+        // ============================
+        // LIST Receiving Receipt By Id
+        // ============================
+        [Route("api/listReceivingReceiptById/{Id}")]
+        public Models.TrnReceivingReceipt GetRRById(String Id)
+        {
+            var RRId = Convert.ToInt32(Id);
+            var receivingReceipts = from d in db.TrnReceivingReceipts
+                                    where d.Id == RRId
+                                    select new Models.TrnReceivingReceipt
+                                    {
+                                        Id = d.Id,
+                                        BranchId = d.BranchId,
+                                        Branch = d.MstBranch.Branch,
+                                        RRDate = d.RRDate.ToShortDateString(),
+                                        RRNumber = d.RRNumber,
+                                        SupplierId = d.SupplierId,
+                                        Supplier = d.MstArticle.Article,
+                                        TermId = d.TermId,
+                                        Term = d.MstTerm.Term,
+                                        DocumentReference = d.DocumentReference,
+                                        ManualRRNumber = d.ManualRRNumber,
+                                        Remarks = d.Remarks,
+                                        Amount = d.Amount,
+                                        WTaxAmount = d.WTaxAmount,
+                                        PaidAmount = d.PaidAmount,
+                                        AdjustmentAmount = d.AdjustmentAmount,
+                                        BalanceAmount = d.BalanceAmount,
+                                        ReceivedById = d.ReceivedById,
+                                        ReceivedBy = d.MstUser4.FullName,
+                                        PreparedById = d.PreparedById,
+                                        PreparedBy = d.MstUser3.FullName,
+                                        CheckedById = d.CheckedById,
+                                        CheckedBy = d.MstUser1.FullName,
+                                        ApprovedById = d.ApprovedById,
+                                        ApprovedBy = d.MstUser.FullName,
+                                        IsLocked = d.IsLocked,
+                                        CreatedById = d.CreatedById,
+                                        CreatedBy = d.MstUser2.FullName,
+                                        CreatedDateTime = d.CreatedDateTime.ToShortDateString(),
+                                        UpdatedById = d.UpdatedById,
+                                        UpdatedBy = d.MstUser5.FullName,
+                                        UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
+                                    };
+            return (Models.TrnReceivingReceipt)receivingReceipts.FirstOrDefault();
+        }
+
         // =========
         // DELETE RR
         // =========
