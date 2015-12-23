da0f03e0aec38718527eef74b506602827872bb0
NoahOliverRigonan <oliverrigonan@gmail.com>
2015-12-23 12:24:06 +0800

    EasyFIS Commit: Sales Filter and other modifications

--- easyfis/Controllers/ApiSalesInvoiceController.cs
+++ easyfis/Controllers/ApiSalesInvoiceController.cs
@@ -100,6 +100,52 @@ namespace easyfis.Controllers
                                 };
             return (Models.TrnSalesInvoice)salesInvoices.FirstOrDefault();
         }
+        
+        // ===================================
+        // GET Sales Invoice Filter by SI Date
+        // ===================================
+        [Route("api/salesInvoiceFilterBySIDate/{SIDate}")]
+        public List<Models.TrnSalesInvoice> GetSalesFilterBySIDate(String SIDate)
+        {
+            var sales_SIDate = Convert.ToDateTime(SIDate);
+            var salesInvoices = from d in db.TrnSalesInvoices
+                                where d.SIDate == sales_SIDate
+                                select new Models.TrnSalesInvoice
+                                {
+                                    Id = d.Id,
+                                    BranchId = d.BranchId,
+                                    Branch = d.MstBranch.Branch,
+                                    SINumber = d.SINumber,
+                                    SIDate = d.SIDate.ToShortDateString(),
+                                    CustomerId = d.CustomerId,
+                                    Customer = d.MstArticle.Article,
+                                    TermId = d.TermId,
+                                    Term = d.MstTerm.Term,
+                                    DocumentReference = d.DocumentReference,
+                                    ManualSINumber = d.ManualSINumber,
+                                    Remarks = d.Remarks,
+                                    Amount = d.Amount,
+                                    PaidAmount = d.PaidAmount,
+                                    AdjustmentAmount = d.AdjustmentAmount,
+                                    BalanceAmount = d.BalanceAmount,
+                                    SoldById = d.SoldById,
+                                    SoldBy = d.MstUser4.FullName,
+                                    PreparedById = d.PreparedById,
+                                    PreparedBy = d.MstUser3.FullName,
+                                    CheckedById = d.CheckedById,
+                                    CheckedBy = d.MstUser1.FullName,
+                                    ApprovedById = d.ApprovedById,
+                                    ApprovedBy = d.MstUser.FullName,
+                                    IsLocked = d.IsLocked,
+                                    CreatedById = d.CreatedById,
+                                    CreatedBy = d.MstUser2.FullName,
+                                    CreatedDateTime = d.CreatedDateTime.ToShortDateString(),
+                                    UpdatedById = d.UpdatedById,
+                                    UpdatedBy = d.MstUser5.FullName,
+                                    UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
+                                };
+            return salesInvoices.ToList();
+        }
 
         // =================================
         // GET last SINumber in SalesInvoice
