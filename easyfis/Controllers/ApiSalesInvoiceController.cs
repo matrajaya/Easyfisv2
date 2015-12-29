1857cd06075103d2e5a057779758bced7dbe91e1
NoahOliverRigonan <oliverrigonan@gmail.com>
2015-12-30 00:49:20 +0800

    EasyFIS Commit: Sales CRUD Partial and other modification

--- easyfis/Controllers/ApiSalesInvoiceController.cs
+++ easyfis/Controllers/ApiSalesInvoiceController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
 namespace easyfis.Controllers
 {
@@ -151,7 +152,7 @@ namespace easyfis.Controllers
         // GET last SINumber in SalesInvoice
         // =================================
         [Route("api/salesInvoiceLastSINumber")]
-        public Models.TrnSalesInvoice GetSalesLastId()
+        public Models.TrnSalesInvoice GetSalesLastSINumber()
         {
             var salesInvoices = from d in db.TrnSalesInvoices.OrderByDescending(d => d.SINumber)
                                 select new Models.TrnSalesInvoice
@@ -189,6 +190,157 @@ namespace easyfis.Controllers
                                     UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
                                 };
             return (Models.TrnSalesInvoice)salesInvoices.FirstOrDefault();
+        }
+
+        // ====================
+        // GET last Id in Sales
+        // ====================
+        [Route("api/salesInvoiceLastId")]
+        public Models.TrnSalesInvoice GetSalesLastId()
+        {
+            var salesInvoices = from d in db.TrnSalesInvoices.OrderByDescending(d => d.Id)
+                                select new Models.TrnSalesInvoice
+                                {
+                                    Id = d.Id,
+                                    BranchId = d.BranchId,
+                                    Branch = d.MstBranch.Branch,
+                                    SINumber = d.SINumber,
+                                    SIDate = d.SIDate.ToShortDateString(),
+                                    CustomerId = d.CustomerId,
+                                    Customer = d.MstArticle.Article,
+                                    TermId = d.TermId,
+                                    Term = d.MstTerm.Term,
+                                    DocumentReference = d.DocumentReference,
+                                    ManualSINumber = d.ManualSINumber,
+                                    Remarks = d.Remarks,
+                                    Amount = d.Amount,
+                                    PaidAmount = d.PaidAmount,
+                                    AdjustmentAmount = d.AdjustmentAmount,
+                                    BalanceAmount = d.BalanceAmount,
+                                    SoldById = d.SoldById,
+                                    SoldBy = d.MstUser4.FullName,
+                                    PreparedById = d.PreparedById,
+                                    PreparedBy = d.MstUser3.FullName,
+                                    CheckedById = d.CheckedById,
+                                    CheckedBy = d.MstUser1.FullName,
+                                    ApprovedById = d.ApprovedById,
+                                    ApprovedBy = d.MstUser.FullName,
+                                    IsLocked = d.IsLocked,
+                                    CreatedById = d.CreatedById,
+                                    CreatedBy = d.MstUser2.FullName,
+                                    CreatedDateTime = d.CreatedDateTime.ToShortDateString(),
+                                    UpdatedById = d.UpdatedById,
+                                    UpdatedBy = d.MstUser5.FullName,
+                                    UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
+                                };
+            return (Models.TrnSalesInvoice)salesInvoices.FirstOrDefault();
+        }
+
+
+        // =========
+        // ADD Sales
+        // =========
+        [Route("api/addSales")]
+        public int Post(Models.TrnSalesInvoice sales)
+        {
+            try
+            {
+                var isLocked = false;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                Data.TrnSalesInvoice newSales = new Data.TrnSalesInvoice();
+
+                newSales.BranchId = sales.BranchId;
+                newSales.SINumber = sales.SINumber;
+                newSales.SIDate = Convert.ToDateTime(sales.SIDate);
+                newSales.CustomerId = sales.CustomerId;
+                newSales.TermId = sales.TermId;
+                newSales.DocumentReference = sales.DocumentReference;
+                newSales.ManualSINumber = sales.ManualSINumber;
+                newSales.Remarks = sales.Remarks;
+                newSales.Amount = sales.Amount;
+                newSales.PaidAmount = sales.PaidAmount;
+                newSales.AdjustmentAmount = sales.AdjustmentAmount;
+                newSales.BalanceAmount = sales.BalanceAmount;
+                newSales.SoldById = sales.SoldById;
+                newSales.PreparedById = sales.PreparedById;
+                newSales.CheckedById = sales.CheckedById;
+                newSales.ApprovedById = sales.ApprovedById;
+
+                newSales.IsLocked = isLocked;
+                newSales.CreatedById = mstUserId;
+                newSales.CreatedDateTime = date;
+                newSales.UpdatedById = mstUserId;
+                newSales.UpdatedDateTime = date;
+
+                db.TrnSalesInvoices.InsertOnSubmit(newSales);
+                db.SubmitChanges();
+
+                return newSales.Id;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        // ==============
+        // UPDATE Company
+        // ==============
+        [Route("api/updateSales/{id}")]
+        public HttpResponseMessage Put(String id, Models.TrnSalesInvoice sales)
+        {
+            try
+            {
+                //var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                var sales_Id = Convert.ToInt32(id);
+                var salesInvoces = from d in db.TrnSalesInvoices where d.Id == sales_Id select d;
+
+                if (salesInvoces.Any())
+                {
+                    var updateSales = salesInvoces.FirstOrDefault();
+
+                    updateSales.BranchId = sales.BranchId;
+                    updateSales.SINumber = sales.SINumber;
+                    updateSales.SIDate = Convert.ToDateTime(sales.SIDate);
+                    updateSales.CustomerId = sales.CustomerId;
+                    updateSales.TermId = sales.TermId;
+                    updateSales.DocumentReference = sales.DocumentReference;
+                    updateSales.ManualSINumber = sales.ManualSINumber;
+                    updateSales.Remarks = sales.Remarks;
+                    updateSales.Amount = sales.Amount;
+                    updateSales.PaidAmount = sales.PaidAmount;
+                    updateSales.AdjustmentAmount = sales.AdjustmentAmount;
+                    updateSales.BalanceAmount = sales.BalanceAmount;
+                    updateSales.SoldById = sales.SoldById;
+                    updateSales.PreparedById = sales.PreparedById;
+                    updateSales.CheckedById = sales.CheckedById;
+                    updateSales.ApprovedById = sales.ApprovedById;
+
+                    updateSales.IsLocked = sales.IsLocked;
+                    updateSales.UpdatedById = mstUserId;
+                    updateSales.UpdatedDateTime = date;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
 
         // ============
