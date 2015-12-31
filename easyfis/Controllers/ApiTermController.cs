ef326b736b99adbd240d3e1da47dbc30e8b42315
NoahOliverRigonan <oliverrigonan@gmail.com>
2016-01-01 02:30:38 +0800

    EasyFIS Commit: CRUD Term (Done) and other modifications

--- easyfis/Controllers/ApiTermController.cs
+++ easyfis/Controllers/ApiTermController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
 namespace easyfis.Controllers
 {
@@ -32,6 +33,110 @@ namespace easyfis.Controllers
                             UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
                         };
             return terms.ToList();
+        }
+
+        // ========
+        // ADD Term
+        // ========
+        [Route("api/addTerm")]
+        public int Post(Models.MstTerm term)
+        {
+            try
+            {
+                var isLocked = false;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                Data.MstTerm newTerm = new Data.MstTerm();
+
+                newTerm.Term = term.Term;
+                newTerm.NumberOfDays = term.NumberOfDays;
+                newTerm.IsLocked = isLocked;
+                newTerm.CreatedById = mstUserId;
+                newTerm.CreatedDateTime = date;
+                newTerm.UpdatedById = mstUserId;
+                newTerm.UpdatedDateTime = date;
+
+                db.MstTerms.InsertOnSubmit(newTerm);
+                db.SubmitChanges();
+
+                return newTerm.Id;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        // ===========
+        // UPDATE Term
+        // ===========
+        [Route("api/updateTerm/{id}")]
+        public HttpResponseMessage Put(String id, Models.MstTerm term)
+        {
+            try
+            {
+                //var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                var term_Id = Convert.ToInt32(id);
+                var terms = from d in db.MstTerms where d.Id == term_Id select d;
+
+                if (terms.Any())
+                {
+                    var updateTerm = terms.FirstOrDefault();
+
+                    updateTerm.Term = term.Term;
+                    updateTerm.NumberOfDays = term.NumberOfDays;
+                    updateTerm.IsLocked = term.IsLocked;
+                    updateTerm.UpdatedById = mstUserId;
+                    updateTerm.UpdatedDateTime = date;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // ===========
+        // DELETE Term
+        // ===========
+        [Route("api/deleteTerm/{id}")]
+        public HttpResponseMessage Delete(String id)
+        {
+            try
+            {
+                var term_Id = Convert.ToInt32(id);
+                var terms = from d in db.MstTerms where d.Id == term_Id select d;
+
+                if (terms.Any())
+                {
+                    db.MstTerms.DeleteOnSubmit(terms.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
