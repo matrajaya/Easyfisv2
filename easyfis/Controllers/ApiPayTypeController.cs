80cbf3a7bbd4aa4a0727972472f53ced6e62baa2
NoahOliverRigonan <oliverrigonan@gmail.com>
2015-12-31 23:13:51 +0800

    EasyFIS Commit: CRUD PayType and TaxType in System Tables (Done)

--- easyfis/Controllers/ApiPayTypeController.cs
+++ easyfis/Controllers/ApiPayTypeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
 namespace easyfis.Controllers
 {
@@ -11,9 +12,9 @@ namespace easyfis.Controllers
     {
         private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
 
-        // ============
-        // LIST PayTpes
-        // ============
+        // =============
+        // LIST Pay Type
+        // =============
         [Route("api/listPayType")]
         public List<Models.MstPayType> Get()
         {
@@ -33,6 +34,111 @@ namespace easyfis.Controllers
                                     UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
                                 };
             return payTypes.ToList();
+        }
+
+        // ============
+        // ADD Pay Type
+        // ============
+        [Route("api/addPayType")]
+        public int Post(Models.MstPayType payType)
+        {
+            try
+            {
+                var isLocked = false;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                Data.MstPayType newPayType = new Data.MstPayType();
+
+                newPayType.PayType = payType.PayType;
+                newPayType.AccountId = payType.AccountId;
+                newPayType.IsLocked = isLocked;
+                newPayType.CreatedById = mstUserId;
+                newPayType.CreatedDateTime = date;
+                newPayType.UpdatedById = mstUserId;
+                newPayType.UpdatedDateTime = date;
+
+                db.MstPayTypes.InsertOnSubmit(newPayType);
+                db.SubmitChanges();
+
+                return newPayType.Id;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        // ===============
+        // UPDATE Pay Type
+        // ===============
+        [Route("api/updatePayType/{id}")]
+        public HttpResponseMessage Put(String id, Models.MstPayType payType)
+        {
+            try
+            {
+                //var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                var payType_Id = Convert.ToInt32(id);
+                var payTypes = from d in db.MstPayTypes where d.Id == payType_Id select d;
+
+                if (payTypes.Any())
+                {
+                    var updatePayType = payTypes.FirstOrDefault();
+
+                    updatePayType.PayType = payType.PayType;
+                    updatePayType.AccountId = payType.AccountId;
+                    updatePayType.IsLocked = payType.IsLocked;
+                    updatePayType.UpdatedById = mstUserId;
+                    updatePayType.UpdatedDateTime = date;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // ===============
+        // DELETE Pay Type
+        // ===============
+        [Route("api/deletePayType/{id}")]
+        public HttpResponseMessage Delete(String id)
+        {
+            try
+            {
+                var payType_Id = Convert.ToInt32(id);
+                var payTypes = from d in db.MstPayTypes where d.Id == payType_Id select d;
+
+                if (payTypes.Any())
+                {
+                    db.MstPayTypes.DeleteOnSubmit(payTypes.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
--- easyfis/Controllers/ApiTaxTypeController.cs
+++ easyfis/Controllers/ApiTaxTypeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
 namespace easyfis.Controllers
 {
@@ -35,6 +36,116 @@ namespace easyfis.Controllers
                                UpdatedDateTime = d.UpdatedDateTime.ToShortDateString()
                            };
             return taxTypes.ToList();
+        }
+
+
+        // ============
+        // ADD Tax Type
+        // ============
+        [Route("api/addTaxType")]
+        public int Post(Models.MstTaxType taxType)
+        {
+            try
+            {
+                var isLocked = false;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                Data.MstTaxType newTaxType = new Data.MstTaxType();
+                
+                newTaxType.TaxType = taxType.TaxType;
+                newTaxType.TaxRate = taxType.TaxRate;
+                newTaxType.IsInclusive = taxType.IsInclusive;
+                newTaxType.AccountId = taxType.AccountId;
+                newTaxType.IsLocked = isLocked;
+                newTaxType.CreatedById = mstUserId;
+                newTaxType.CreatedDateTime = date;
+                newTaxType.UpdatedById = mstUserId;
+                newTaxType.UpdatedDateTime = date;
+
+                db.MstTaxTypes.InsertOnSubmit(newTaxType);
+                db.SubmitChanges();
+
+                return newTaxType.Id;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        // ===============
+        // UPDATE Tax Type
+        // ===============
+        [Route("api/updateTaxType/{id}")]
+        public HttpResponseMessage Put(String id, Models.MstTaxType taxType)
+        {
+            try
+            {
+                //var isLocked = true;
+                var identityUserId = User.Identity.GetUserId();
+                var mstUserId = (from d in db.MstUsers where d.UserId == identityUserId select d.Id).SingleOrDefault();
+                var date = DateTime.Now;
+
+                var taxType_Id = Convert.ToInt32(id);
+                var taxTypes = from d in db.MstTaxTypes where d.Id == taxType_Id select d;
+
+                if (taxTypes.Any())
+                {
+                    var updateTaxType = taxTypes.FirstOrDefault();
+
+                    updateTaxType.TaxType = taxType.TaxType;
+                    updateTaxType.TaxRate = taxType.TaxRate;
+                    updateTaxType.IsInclusive = taxType.IsInclusive;
+                    updateTaxType.AccountId = taxType.AccountId;
+                    updateTaxType.IsLocked = taxType.IsLocked;
+                    updateTaxType.UpdatedById = mstUserId;
+                    updateTaxType.UpdatedDateTime = date;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // ===============
+        // DELETE Tax Type
+        // ===============
+        [Route("api/deleteTaxType/{id}")]
+        public HttpResponseMessage Delete(String id)
+        {
+            try
+            {
+                var taxType_Id = Convert.ToInt32(id);
+                var taxTypes = from d in db.MstTaxTypes where d.Id == taxType_Id select d;
+
+                if (taxTypes.Any())
+                {
+                    db.MstTaxTypes.DeleteOnSubmit(taxTypes.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
