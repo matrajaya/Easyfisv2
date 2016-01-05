75b911cddf887d8cbd516ab3b2825affc071a202
NoahOliverRigonan <oliverrigonan@gmail.com>
2016-01-05 15:06:50 -0800

    EasyFIS Commit: CRUD POLine Items

--- easyfis/Controllers/ApiPurchaseOrderItemController.cs
+++ easyfis/Controllers/ApiPurchaseOrderItemController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using System.Diagnostics;
 
 namespace easyfis.Controllers
 {
@@ -61,6 +63,103 @@ namespace easyfis.Controllers
                                          Amount = d.Amount
                                      };
             return PurchaseOrderItems.ToList();
+        }
+
+        // =======================
+        // ADD Purchase Order Item
+        // =======================
+        [Route("api/addPurchaseOrderItem")]
+        public int Post(Models.TrnPurchaseOrderItem purchaseOrderItem)
+        {
+            try
+            {
+                Data.TrnPurchaseOrderItem newPurchaseOrderItem = new Data.TrnPurchaseOrderItem();
+
+                newPurchaseOrderItem.POId = purchaseOrderItem.POId;
+                newPurchaseOrderItem.ItemId = purchaseOrderItem.ItemId;
+                newPurchaseOrderItem.Particulars = purchaseOrderItem.Particulars;
+                newPurchaseOrderItem.UnitId = purchaseOrderItem.UnitId;
+                newPurchaseOrderItem.Quantity = purchaseOrderItem.Quantity;
+                newPurchaseOrderItem.Cost = purchaseOrderItem.Cost;
+                newPurchaseOrderItem.Amount = purchaseOrderItem.Amount;
+
+                db.TrnPurchaseOrderItems.InsertOnSubmit(newPurchaseOrderItem);
+                db.SubmitChanges();
+
+                return newPurchaseOrderItem.Id;
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e);
+                return 0;
+            }
+        }
+
+        // ==========================
+        // UPDATE Purchase Order Item
+        // ==========================
+        [Route("api/updatePurchaseOrderItem/{id}")]
+        public HttpResponseMessage Put(String id, Models.TrnPurchaseOrderItem purchaseOrderItem)
+        {
+            try
+            {
+                var purchaseOrderItem_Id = Convert.ToInt32(id);
+                var purchaseOrderItems = from d in db.TrnPurchaseOrderItems where d.Id == purchaseOrderItem_Id select d;
+
+                if (purchaseOrderItems.Any())
+                {
+                    var updatePurchaseOrderItem = purchaseOrderItems.FirstOrDefault();
+
+                    updatePurchaseOrderItem.POId = purchaseOrderItem.POId;
+                    updatePurchaseOrderItem.ItemId = purchaseOrderItem.ItemId;
+                    updatePurchaseOrderItem.Particulars = purchaseOrderItem.Particulars;
+                    updatePurchaseOrderItem.UnitId = purchaseOrderItem.UnitId;
+                    updatePurchaseOrderItem.Quantity = purchaseOrderItem.Quantity;
+                    updatePurchaseOrderItem.Cost = purchaseOrderItem.Cost;
+                    updatePurchaseOrderItem.Amount = purchaseOrderItem.Amount;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // ==========================
+        // DELETE Purchase Order Item
+        // ==========================
+        [Route("api/deletePurchaseOrderItem/{id}")]
+        public HttpResponseMessage Delete(String id)
+        {
+            try
+            {
+                var purchaseOrderItem_Id = Convert.ToInt32(id);
+                var purchaseOrderItems = from d in db.TrnPurchaseOrderItems where d.Id == purchaseOrderItem_Id select d;
+
+                if (purchaseOrderItems.Any())
+                {
+                    db.TrnPurchaseOrderItems.DeleteOnSubmit(purchaseOrderItems.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
