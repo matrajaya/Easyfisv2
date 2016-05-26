194b848e9be7340dfb75bd0244825f323358a20f
NoahOliverRigonan <oliverrigonan@gmail.com>
2016-05-25 17:23:28 -0800

    EasyFIS Commit: All input empty fields is defaulted by "NA"

--- easyfis/Scripts/software.js
+++ easyfis/Scripts/software.js
@@ -1,40 +1,4 @@
 $(document).ready(function () {
-    // Collapse 1 in set ups
-    $('#collapse1').on('shown.bs.collapse', function () {
-        $(".fa-drop1").addClass('fa-chevron-up').removeClass('fa-chevron-down');
-    });
-
-    $('#collapse1').on('hidden.bs.collapse', function () {
-        $(".fa-drop1").addClass('fa-chevron-down').removeClass('fa-chevron-up');
-    });
-
-    // collapse 2 in systmes
-    $('#collapse2').on('shown.bs.collapse', function () {
-        $(".fa-drop2").addClass('fa-chevron-up').removeClass('fa-chevron-down');
-    });
-
-    $('#collapse2').on('hidden.bs.collapse', function () {
-        $(".fa-drop2").addClass('fa-chevron-down').removeClass('fa-chevron-up');
-    });
-
-    // collapse 3 in activity
-    $('#collapse3').on('shown.bs.collapse', function () {
-        $(".fa-drop3").addClass('fa-chevron-up').removeClass('fa-chevron-down');
-    });
-
-    $('#collapse3').on('hidden.bs.collapse', function () {
-        $(".fa-drop3").addClass('fa-chevron-down').removeClass('fa-chevron-up');
-    });
-
-    // collapse 4 in reports
-    $('#collapse4').on('shown.bs.collapse', function () {
-        $(".fa-drop4").addClass('fa-chevron-up').removeClass('fa-chevron-down');
-    });
-
-    $('#collapse4').on('hidden.bs.collapse', function () {
-        $(".fa-drop4").addClass('fa-chevron-down').removeClass('fa-chevron-up');
-    });
-
     // number fields
     function RemoveRougeChar(convertString) {
         if (convertString.substring(0, 1) == ",") {
