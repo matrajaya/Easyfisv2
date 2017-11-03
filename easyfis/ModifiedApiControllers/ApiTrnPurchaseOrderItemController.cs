8675e1c845175a825e5da12dfba46bee881b4ad5
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2017-11-03 14:15:19 +0800

    Purchase Order Item Changes (HttpDelete)

--- easyfis/ModifiedApiControllers/ApiTrnPurchaseOrderItemController.cs
+++ easyfis/ModifiedApiControllers/ApiTrnPurchaseOrderItemController.cs
@@ -125,7 +125,7 @@ namespace easyfis.ModifiedApiControllers
         // Add Purchase Order Item
         // =======================
         [Authorize, HttpPost, Route("api/purchaseOrderItem/add/{POId}")]
-        public HttpResponseMessage AddUnitConversion(Entities.TrnPurchaseOrderItem objPurchaseOrderItem, String POId)
+        public HttpResponseMessage AddPurchaseOrderItem(Entities.TrnPurchaseOrderItem objPurchaseOrderItem, String POId)
         {
             try
             {
@@ -176,7 +176,7 @@ namespace easyfis.ModifiedApiControllers
                                             Decimal baseCost = objPurchaseOrderItem.Amount;
                                             if (baseQuantity > 0)
                                             {
-                                                baseQuantity = objPurchaseOrderItem.Amount / baseQuantity;
+                                                baseCost = objPurchaseOrderItem.Amount / baseQuantity;
                                             }
 
                                             Data.TrnPurchaseOrderItem newPurchaseOrderItem = new Data.TrnPurchaseOrderItem
@@ -190,7 +190,7 @@ namespace easyfis.ModifiedApiControllers
                                                 Amount = objPurchaseOrderItem.Amount,
                                                 BaseUnitId = itemUnit.FirstOrDefault().UnitId,
                                                 BaseQuantity = baseQuantity,
-
+                                                BaseCost = baseCost
                                             };
 
                                             db.TrnPurchaseOrderItems.InsertOnSubmit(newPurchaseOrderItem);
@@ -240,5 +240,212 @@ namespace easyfis.ModifiedApiControllers
             }
         }
 
+        // ==========================
+        // Update Purchase Order Item
+        // ==========================
+        [Authorize, HttpPut, Route("api/purchaseOrderItem/update/{id}/{POId}")]
+        public HttpResponseMessage UpdatePurchaseOrderItem(Entities.TrnPurchaseOrderItem objPurchaseOrderItem, String id, String POId)
+        {
+            try
+            {
+                var currentUser = from d in db.MstUsers
+                                  where d.UserId == User.Identity.GetUserId()
+                                  select d;
+
+                if (currentUser.Any())
+                {
+                    var currentUserId = currentUser.FirstOrDefault().Id;
+
+                    var userForms = from d in db.MstUserForms
+                                    where d.UserId == currentUserId
+                                    && d.SysForm.FormName.Equals("PurchaseOrderDetail")
+                                    select d;
+
+                    if (userForms.Any())
+                    {
+                        if (userForms.FirstOrDefault().CanEdit)
+                        {
+                            var purchaseOrder = from d in db.TrnPurchaseOrders
+                                                where d.Id == Convert.ToInt32(POId)
+                                                select d;
+
+                            if (purchaseOrder.Any())
+                            {
+                                if (!purchaseOrder.FirstOrDefault().IsLocked)
+                                {
+                                    var purchaseOrderItem = from d in db.TrnPurchaseOrderItems
+                                                            where d.Id == Convert.ToInt32(id)
+                                                            select d;
+
+                                    if (purchaseOrderItem.Any())
+                                    {
+                                        var itemUnit = from d in db.MstArticles
+                                                       where d.Id == objPurchaseOrderItem.ItemId
+                                                       select d;
+
+                                        if (itemUnit.Any())
+                                        {
+                                            var conversionUnit = from d in db.MstArticleUnits
+                                                                 where d.ArticleId == objPurchaseOrderItem.ItemId
+                                                                 && d.UnitId == objPurchaseOrderItem.UnitId
+                                                                 select d;
+
+                                            if (conversionUnit.Any())
+                                            {
+                                                Decimal baseQuantity = objPurchaseOrderItem.Quantity * 1;
+                                                if (conversionUnit.FirstOrDefault().Multiplier > 0)
+                                                {
+                                                    baseQuantity = objPurchaseOrderItem.Quantity * (1 / conversionUnit.FirstOrDefault().Multiplier);
+                                                }
+
+                                                Decimal baseCost = objPurchaseOrderItem.Amount;
+                                                if (baseQuantity > 0)
+                                                {
+                                                    baseCost = objPurchaseOrderItem.Amount / baseQuantity;
+                                                }
+
+                                                var updatePurchaseOrdeItem = purchaseOrderItem.FirstOrDefault();
+                                                updatePurchaseOrdeItem.ItemId = objPurchaseOrderItem.ItemId;
+                                                updatePurchaseOrdeItem.Particulars = objPurchaseOrderItem.Particulars;
+                                                updatePurchaseOrdeItem.UnitId = objPurchaseOrderItem.UnitId;
+                                                updatePurchaseOrdeItem.Quantity = objPurchaseOrderItem.Quantity;
+                                                updatePurchaseOrdeItem.Cost = objPurchaseOrderItem.Cost;
+                                                updatePurchaseOrdeItem.Amount = objPurchaseOrderItem.Amount;
+                                                updatePurchaseOrdeItem.BaseUnitId = itemUnit.FirstOrDefault().UnitId;
+                                                updatePurchaseOrdeItem.BaseQuantity = baseQuantity;
+                                                updatePurchaseOrdeItem.BaseUnitId = itemUnit.FirstOrDefault().UnitId;
+                                                updatePurchaseOrdeItem.BaseCost = baseCost;
+
+                                                db.SubmitChanges();
+
+                                                return Request.CreateResponse(HttpStatusCode.OK);
+                                            }
+                                            else
+                                            {
+                                                return Request.CreateResponse(HttpStatusCode.BadRequest, "The selected item has no unit conversion.");
+                                            }
+                                        }
+                                        else
+                                        {
+                                            return Request.CreateResponse(HttpStatusCode.BadRequest, "The selected item was not found in the server.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        return Request.CreateResponse(HttpStatusCode.NotFound, "This purchase order item detail is no longer exist in the server.");
+                                    }
+                                }
+                                else
+                                {
+                                    return Request.CreateResponse(HttpStatusCode.BadRequest, "You cannot update purchase order item if the current purchase order detail is locked.");
+                                }
+                            }
+                            else
+                            {
+                                return Request.CreateResponse(HttpStatusCode.NotFound, "These current purchase order details are not found in the server. Please add new purchase order first before proceeding.");
+                            }
+                        }
+                        else
+                        {
+                            return Request.CreateResponse(HttpStatusCode.BadRequest, "Sorry. You have no rights to update purchase order item in this purchase order detail page.");
+                        }
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.BadRequest, "Sorry. You have no access in this purchase order detail page.");
+                    }
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Theres no current user logged in.");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Something's went wrong from the server.");
+            }
+        }
+
+        // ==========================
+        // Delete Purchase Order Item
+        // ==========================
+        [Authorize, HttpDelete, Route("api/purchaseOrderItem/delete/{id}/{POId}")]
+        public HttpResponseMessage DeletePurchaseOrderItem(String id, String POId)
+        {
+            try
+            {
+                var currentUser = from d in db.MstUsers
+                                  where d.UserId == User.Identity.GetUserId()
+                                  select d;
+
+                if (currentUser.Any())
+                {
+                    var currentUserId = currentUser.FirstOrDefault().Id;
+
+                    var userForms = from d in db.MstUserForms
+                                    where d.UserId == currentUserId
+                                    && d.SysForm.FormName.Equals("PurchaseOrderDetail")
+                                    select d;
+
+                    if (userForms.Any())
+                    {
+                        if (userForms.FirstOrDefault().CanDelete)
+                        {
+                            var purchaseOrder = from d in db.TrnPurchaseOrders
+                                                where d.Id == Convert.ToInt32(POId)
+                                                select d;
+
+                            if (purchaseOrder.Any())
+                            {
+                                if (!purchaseOrder.FirstOrDefault().IsLocked)
+                                {
+                                    var purchaseOrderItem = from d in db.TrnPurchaseOrderItems
+                                                            where d.Id == Convert.ToInt32(id)
+                                                            select d;
+
+                                    if (purchaseOrderItem.Any())
+                                    {
+                                        db.TrnPurchaseOrderItems.DeleteOnSubmit(purchaseOrderItem.First());
+                                        db.SubmitChanges();
+
+                                        return Request.CreateResponse(HttpStatusCode.OK);
+                                    }
+                                    else
+                                    {
+                                        return Request.CreateResponse(HttpStatusCode.NotFound, "This purchase order item detail is no longer exist in the server.");
+                                    }
+                                }
+                                else
+                                {
+                                    return Request.CreateResponse(HttpStatusCode.BadRequest, "You cannot delete purchase order item if the current purchase order detail is locked.");
+                                }
+                            }
+                            else
+                            {
+                                return Request.CreateResponse(HttpStatusCode.NotFound, "These current purchase order details are not found in the server. Please add new purchase order first before proceeding.");
+                            }
+                        }
+                        else
+                        {
+                            return Request.CreateResponse(HttpStatusCode.BadRequest, "Sorry. You have no rights to delete purchase order item in this purchase order item detail page.");
+                        }
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.BadRequest, "Sorry. You have no access in this purchase order detail page.");
+                    }
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Theres no current user logged in.");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Something's went wrong from the server.");
+            }
+        }
     }
 }
