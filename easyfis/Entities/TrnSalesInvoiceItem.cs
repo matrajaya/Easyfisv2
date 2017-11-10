85f54302565e92551ff9c9d7c2c738d2a0ede164
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2017-11-10 15:26:53 +0800

    Update Sales Invoice Item - API

--- easyfis/Entities/TrnSalesInvoiceItem.cs
+++ easyfis/Entities/TrnSalesInvoiceItem.cs
@@ -10,20 +10,27 @@ namespace easyfis.Entities
         public Int32 Id { get; set; }
         public Int32 SIId { get; set; }
         public Int32 ItemId { get; set; }
-        public Int32 ItemInventoryId { get; set; }
+        public String ItemCode { get; set; }
+        public String ItemDescription { get; set; }
         public String Particulars { get; set; }
+        public Int32? ItemInventoryId { get; set; }
+        public String ItemInventoryCode { get; set; }
         public Int32 UnitId { get; set; }
+        public String Unit { get; set; }
         public Decimal Quantity { get; set; }
         public Decimal Price { get; set; }
         public Int32 DiscountId { get; set; }
+        public String Discount { get; set; }
         public Decimal DiscountRate { get; set; }
         public Decimal DiscountAmount { get; set; }
         public Decimal NetPrice { get; set; }
         public Decimal Amount { get; set; }
         public Int32 VATId { get; set; }
+        public String VAT { get; set; }
         public Decimal VATPercentage { get; set; }
         public Decimal VATAmount { get; set; }
         public Int32 BaseUnitId { get; set; }
+        public String BaseUnit { get; set; }
         public Decimal BaseQuantity { get; set; }
         public Decimal BasePrice { get; set; }
         public String SalesItemTimeStamp { get; set; }
--- easyfis/ModifiedApiControllers/ApiTrnSalesInvoiceItemController.cs
+++ easyfis/ModifiedApiControllers/ApiTrnSalesInvoiceItemController.cs
@@ -4,11 +4,181 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using System.Diagnostics;
 
 namespace easyfis.ModifiedApiControllers
 {
     public class ApiTrnSalesInvoiceItemController : ApiController
     {
+        // ============
+        // Data Context
+        // ============
+        private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+        // =======================
+        // List Sales Invoice Item
+        // =======================
+        [Authorize, HttpGet, Route("api/salesInvoiceItem/list/{SIId}")]
+        public List<Entities.TrnSalesInvoiceItem> ListSalesInvoiceItem(String SIId)
+        {
+            var salesInvoiceItems = from d in db.TrnSalesInvoiceItems
+                                    where d.SIId == Convert.ToInt32(SIId)
+                                    select new Entities.TrnSalesInvoiceItem
+                                    {
+                                        Id = d.Id,
+                                        SIId = d.SIId,
+                                        ItemId = d.ItemId,
+                                        ItemCode = d.MstArticle.ManualArticleCode,
+                                        ItemDescription = d.MstArticle.Article,
+                                        Particulars = d.Particulars,
+                                        ItemInventoryId = d.ItemInventoryId,
+                                        ItemInventoryCode = d.ItemInventoryId != null ? d.MstArticleInventory.InventoryCode : "",
+                                        Quantity = d.Quantity,
+                                        UnitId = d.UnitId,
+                                        Unit = d.MstUnit1.Unit,
+                                        Price = d.Price,
+                                        DiscountId = d.DiscountId,
+                                        Discount = d.MstDiscount.Discount,
+                                        DiscountRate = d.DiscountRate,
+                                        DiscountAmount = d.DiscountAmount,
+                                        NetPrice = d.NetPrice,
+                                        Amount = d.Amount,
+                                        VATId = d.VATId,
+                                        VAT = d.MstTaxType.TaxType,
+                                        VATPercentage = d.VATPercentage,
+                                        VATAmount = d.VATAmount,
+                                        BaseUnitId = d.BaseUnitId,
+                                        BaseUnit = d.MstUnit.Unit,
+                                        BaseQuantity = d.BaseQuantity,
+                                        BasePrice = d.BasePrice
+                                    };
+
+            return salesInvoiceItems.ToList();
+        }
+
+        // ============================
+        // Dropdown List - Item (Field)
+        // ============================
+        [Authorize, HttpGet, Route("api/salesInvoiceItem/dropdown/list/item")]
+        public List<Entities.MstArticle> DropdownListSalesInvoiceItemListItem()
+        {
+            var items = from d in db.MstArticles.OrderBy(d => d.Article)
+                        where d.ArticleTypeId == 1
+                        && d.IsLocked == true
+                        select new Entities.MstArticle
+                        {
+                            Id = d.Id,
+                            ManualArticleCode = d.ManualArticleCode,
+                            Article = d.Article
+                        };
+
+            return items.ToList();
+        }
+
+        // ===========================================
+        // Dropdown List - Item Inventory Code (Field)
+        // ===========================================
+        [Authorize, HttpGet, Route("api/salesInvoiceItem/dropdown/list/itemInventoryCode/{itemId}")]
+        public List<Entities.MstArticleInventory> DropdownListSalesInvoiceItemInventoryCode(String itemId)
+        {
+            var currentUser = from d in db.MstUsers
+                              where d.UserId == User.Identity.GetUserId()
+                              select d;
+
+            var branchId = currentUser.FirstOrDefault().BranchId;
+
+            var itemInventories = from d in db.MstArticleInventories
+                                  where d.BranchId == branchId
+                                  && d.ArticleId == Convert.ToInt32(itemId)
+                                  && d.MstArticle.ArticleTypeId == 1
+                                  && d.MstArticle.IsInventory == true
+                                  && d.MstArticle.IsLocked == true
+                                  select new Entities.MstArticleInventory
+                                  {
+                                      Id = d.Id,
+                                      InventoryCode = d.InventoryCode
+                                  };
+
+            return itemInventories.ToList();
+        }
+
+        // ============================
+        // Dropdown List - Unit (Field)
+        // ============================
+        [Authorize, HttpGet, Route("api/salesInvoiceItem/dropdown/list/itemUnit/{itemId}")]
+        public List<Entities.MstArticleUnit> DropdownListSalesInvoiceItemUnit(String itemId)
+        {
+            var itemUnits = from d in db.MstArticleUnits.OrderBy(d => d.MstUnit.Unit)
+                            where d.ArticleId == Convert.ToInt32(itemId)
+                            && d.MstArticle.IsLocked == true
+                            select new Entities.MstArticleUnit
+                            {
+                                Id = d.Id,
+                                UnitId = d.UnitId,
+                                Unit = d.MstUnit.Unit
+                            };
+
+            return itemUnits.ToList();
+        }
+
+        // =============================
+        // Dropdown List - Price (Field)
+        // =============================
+        [Authorize, HttpGet, Route("api/salesInvoiceItem/dropdown/list/itemPrice/{itemId}")]
+        public List<Entities.MstArticlePrice> DropdownListSalesInvoiceItemPrice(String itemId)
+        {
+            var itemPrices = from d in db.MstArticlePrices.OrderBy(d => d.PriceDescription)
+                             where d.ArticleId == Convert.ToInt32(itemId)
+                             && d.MstArticle.IsLocked == true
+                             select new Entities.MstArticlePrice
+                             {
+                                 Id = d.Id,
+                                 PriceDescription = d.PriceDescription,
+                                 Price = d.Price
+                             };
+
+            return itemPrices.ToList();
+        }
+
+        // ================================
+        // Dropdown List - Discount (Field)
+        // ================================
+        [Authorize, HttpGet, Route("api/salesInvoiceItem/dropdown/list/discount")]
+        public List<Entities.MstDiscount> DropdownListSalesInvoiceItemDiscount()
+        {
+            var discounts = from d in db.MstDiscounts.OrderBy(d => d.Discount)
+                            where d.IsLocked == true
+                            select new Entities.MstDiscount
+                            {
+                                Id = d.Id,
+                                Discount = d.Discount,
+                                DiscountRate = d.DiscountRate,
+                                IsInclusive = d.IsInclusive
+                            };
+
+            return discounts.ToList();
+        }
+
+        // ===========================
+        // Dropdown List - TAX (Field)
+        // ===========================
+        [Authorize, HttpGet, Route("api/salesInvoiceItem/dropdown/list/TAX")]
+        public List<Entities.MstTaxType> DropdownListSalesInvoiceItemTAX()
+        {
+            var taxTypes = from d in db.MstTaxTypes
+                           where d.IsLocked == true
+                           select new Entities.MstTaxType
+                           {
+                               Id = d.Id,
+                               TaxType = d.TaxType,
+                               TaxRate = d.TaxRate,
+                               IsInclusive = d.IsInclusive
+                           };
+
+            return taxTypes.ToList();
+        }
+
 
     }
 }
